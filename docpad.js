0966bd6e8ca2488905c73b880769b4965669fc06
Gabriel Tibúrcio <tibuurcio@gmail.com>
2015-03-05 12:36:17 -0300

    Cronograma e Palestrantes completo.

--- docpad.js
+++ docpad.js
@@ -59,15 +59,22 @@ module.exports = {
     // The entire schedule
     schedule: [
       {
+      	name: "Check-in / Abertura",
       	local: "Anfiteatro D",
-        name: "Check-in / Abertura",
-        time: "8h00 às 8h50"
+      	photo: "themes/yellow-swan/img/arduino.jpg",
+      	time: "8h00 às 8h50",
+      	description: "-"
+      	//presentation: {
+      	//local: "Anfiteatro D",
+        //name: "Check-in / Abertura",
+        //time: "8h00 às 8h50"
+    	//}
       },
       {
         name: "Profº Ivanês Oliveira Alexandrino e Profº Dr. Antônio Pereira Júnior",
         photo: "themes/yellow-swan/img/ivanesoliveira.jpg",
         bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-        company: "Anfiteatro A",
+        company: "Don't worry, be happy.",
 
         presentation: {
    		  name: "Mesa-redonda",	
@@ -78,188 +85,170 @@ module.exports = {
         }
       },
       {
-        name: "Oficina",
-        photo: "themes/yellow-swan/img/arduino.jpg",
-        bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-        company: "Sala 1",
-
-        presentation: {
-          title: "Oficina A",
-          description: "Básico (A1)",
-          time: "9h00 às 10h30"
-        }
-      },
-      {
-        name: "Oficina",
-        photo: "themes/yellow-swan/img/arduino.jpg",
-        bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-        company: "Sala 3",
-
-        presentation: {
-          title: "Oficina B",
-          description: "Básico (A2)",
-          time: "9h00 às 10h30"
-        }
-      },
-      {
         name: "Profº Ivanilson França Vieira Júnior",
         photo: "themes/yellow-swan/img/avatar.jpeg",
-        bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-        company: "Anfiteatro C",
+        bio: "Biografia",
+        company: "Don't worry, be happy.",
 
         presentation: {
-          title: "Palestra 2",
-          description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-          time: "9h00 às 9h50"
+   		  name: "Palestra",	
+          local: "Anfiteatro F",
+          palestrante: "Profº Ivanilson França Vieira Júnior",
+          time: "9h00 às 9h35",
+          description: "Descrição"
         }
       },
       {
-        name: "LAR",
+        name: "Oficina A",
+      	local: "Sala 1",
+      	photo: "themes/yellow-swan/img/arduino.jpg",
+      	time: "9h00 às 11h00",
+      	description: "Básico (A1)"
+      },
+      {
+        name: "Profº Ivanilson França Vieira Júnior e LAR",
         photo: "themes/yellow-swan/img/avatar.jpeg",
-        bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-        company: "Anfiteatro C",
+        bio: "Biografia",
+        company: "Don't worry, be happy.",
+
         presentation: {
-          title: "Palestra 3",
-          description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-          time: "9h40 às 10h15"
-        }
-      },
-      {
-        name: "Profº Dr. Antônio Pereira Júnior",
-        photo: "themes/yellow-swan/img/antoniopereira.jpg",
-        bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-        company: "Anfiteatro A",
-        presentation: {
-          title: "Palestra 4",
-          description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-          time: "10h00 às 10h40"
+   		  name: "Mesa-redonda",	
+          local: "Anfiteatro F",
+          palestrante: "Profº Ivanilson França Vieira Júnior e LAR",
+          time: "9h40 às 10h15",
+          description: "Descrição"
         }
       },
       {
         name: "Tiago Tobias Freitas",
         photo: "themes/yellow-swan/img/avatar.jpeg",
-        bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-        company: "Anfiteatro C",
+        bio: "Biografia",
+        company: "Don't worry, be happy.",
+
         presentation: {
-          title: "Palestra 5",
-          description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-          time: "10h30 às 11h00"
+   		  name: "Palestra",	
+          local: "Anfiteatro F",
+          palestrante: "Tiago Tobias Freitas",
+          time: "10h30 às 11h00",
+          description: "Descrição"
         }
       },
       {
         name: "Profº Dr. Aquiles Medeiros Filgueira Burlamaqui",
         photo: "themes/yellow-swan/img/aquilesmedeiros.jpg",
-        bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-        company: "Anfiteatro A",
+        bio: "Biografia",
+        company: "Don't worry, be happy.",
+
         presentation: {
-          title: "Palestra 6",
-          description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-          time: "10h45 às 11h25"
+   		  name: "Palestra",	
+          local: "Anfiteatro D",
+          palestrante: "Profº Dr. Aquiles Medeiros Filgueira Burlamaqui",
+          time: "10h45 às 11h25",
+          description: "Descrição"
         }
       },
       {
         name: "Almoço",
-        time: "11h25 às 13h00"
+      	photo: "themes/yellow-swan/img/arduino.jpg",
+      	time: "11h25 às 13h00",
+      	description: "-"
       },
       {
         name: "Marcel da Câmara Ribeiro Dantas",
         photo: "themes/yellow-swan/img/marcelribeiro.jpg",
-        bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-        company: "Anfiteatro A",
+        bio: "Biografia",
+        company: "Don't worry, be happy.",
+
         presentation: {
-          title: "Palestra 7",
-          description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-          time: "13h00 às 13h30"
+   		  name: "Palestra",	
+          local: "Anfiteatro D",
+          palestrante: "Marcel da Câmara Ribeiro Dantas",
+          time: "13h00 às 13h30",
+          description: "Descrição"
         }
       },
       {
-        name: "Oficina",
-        photo: "themes/yellow-swan/img/arduino.jpg",
-        bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-        company: "Sala 1",
-
-        presentation: {
-          title: "Oficina C",
-          description: "Intermediário",
-          time: "9h00 às 10h30"
-        }
+        name: "Oficina B",
+      	local: "Sala 1",
+      	photo: "themes/yellow-swan/img/arduino.jpg",
+      	time: "13h00 às 15h00",
+      	description: "Básico (A2)"
       },
       {
-        name: "Oficina",
-        photo: "themes/yellow-swan/img/arduino.jpg",
-        bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-        company: "Sala 3",
+        name: "Oficina C",
+      	local: "Sala 1",
+      	photo: "themes/yellow-swan/img/arduino.jpg",
+      	time: "13h00 às 15h00",
+      	description: "Intermediário"
+      },
+      {
+        name: "Potilive e Pyladies",
+        photo: "themes/yellow-swan/img/pyladies.png",
+        bio: "Biografia",
+        company: "Don't worry, be happy.",
 
         presentation: {
-          title: "Oficina D",
-          description: "Competição",
-          time: "9h00 às 10h30"
+   		  name: "Mesa-redonda",	
+          local: "Anfiteatro D",
+          palestrante: "Potilive e Pyladies",
+          time: "13h35 às 14h15",
+          description: "Descrição"
         }
       },
       {
         name: "Profº Dr. João Moreno Vilas Boas de Souza Silva",
         photo: "themes/yellow-swan/img/joaomoreno.png",
         bio: "Professor efetivo do IFRN com interesse nas áreas de Eletrônica Digital e Microcontroladores.",
-        company: "Anfiteatro C",
+        company: "Don't worry, be happy.",
+
         presentation: {
-          title: "Projetando com arduino: Dos drones à domática",
-          description: "Pensem em um mundo com pequenas aeronaves não tripuladas cruzando nosso espaço aéreo, capazes de realizar até as tarefas mais banais. Agora imaginem casas totalmente conectadas aos nossos smartphones por meio da internet!  O que até décadas atrás poderia até ser considerada uma obra de ficção científica, hoje pode ser algo tão comum como pedir uma pizza ou mesmo ligar uma televisão. Os Drones e a Domótica se tornaram mais do que apenas “termos da moda” para ocupar um lugar de cada vez mais destaque em nosso estilo de vida. Seria ainda melhor, se pudéssemos desenvolver esses projetos com dispositivos de fácil programação, prototipação e baixo custo, não é verdade?! Bem vindo ao presente com a plataforma Arduino…",
-          time: "13h40 às 14h20"
+   		  name: "Projetando com arduino: Dos drones à domática",	
+          local: "Anfiteatro F",
+          palestrante: "Profº Dr. João Moreno Vilas Boas de Souza Silva",
+          time: "13h40 às 14h20",
+          description: "Pensem em um mundo com pequenas aeronaves não tripuladas cruzando nosso espaço aéreo, capazes de realizar até as tarefas mais banais. Agora imaginem casas totalmente conectadas aos nossos smartphones por meio da internet!  O que até décadas atrás poderia até ser considerada uma obra de ficção científica, hoje pode ser algo tão comum como pedir uma pizza ou mesmo ligar uma televisão. Os Drones e a Domótica se tornaram mais do que apenas “termos da moda” para ocupar um lugar de cada vez mais destaque em nosso estilo de vida. Seria ainda melhor, se pudéssemos desenvolver esses projetos com dispositivos de fácil programação, prototipação e baixo custo, não é verdade?! Bem vindo ao presente com a plataforma Arduino…"
         }
       },
       {
-        name: "Potilivre/Pyladies",
-        photo: "themes/yellow-swan/img/pyladies.png",
-        bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-        company: "Anfiteatro A",
+        name: "Julio Cesar Paulino de Melo (LAR)",
+        photo: "themes/yellow-swan/img/joaomoreno.png",
+        bio: "Biografia",
+        company: "Don't worry, be happy.",
+
         presentation: {
-          title: "Palestra 9",
-          description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-          time: "13h35 às 14h14"
+   		  name: "Palestra",	
+          local: "Anfiteatro D",
+          palestrante: "Julio Cesar Paulino de Melo (LAR)",
+          time: "14h30 às 15h00",
+          description: "Descrição"
         }
       },
       {
-        name: "Tiago Hérique Silva de Almeida",
-        photo: "themes/yellow-swan/img/avatar.jpeg",
-        bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-        company: "Anfiteatro C",
+        name: "Tiago Hérique Silva de Almeida e Eugênio Pacelly Brandão Araújo",
+        photo: "themes/yellow-swan/img/avatar.png",
+        bio: "Biografia",
+        company: "Don't worry, be happy.",
+
         presentation: {
-          title: "Palestra 10",
-          description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-          time: "14h30 às 15h00"
-        }
-      },
-      {
-        name: "Eugênio Pacelly Brandão Araújo",
-        photo: "themes/yellow-swan/img/avatar.jpeg",
-        bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-        company: "Anfiteatro C",
-        presentation: {
-          title: "Palestra 11",
-          description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-          time: "14h30 às 15h00"
-        }
-      },
-      {
-        name: "LAR",
-        photo: "themes/yellow-swan/img/avatar.jpeg",
-        bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-        company: "Anfiteatro A",
-        presentation: {
-          title: "Palestra 12",
-          description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-          time: "14h30 às 15h00"
+   		  name: "Mesa-redonda",	
+          local: "Anfiteatro F",
+          palestrante: "Tiago Hérique Silva de Almeida e Eugênio Pacelly Brandão Araújo",
+          time: "14h30 às 15h00",
+          description: "Descrição"
         }
       },
       {
         name: "Profº Dr. Leonardo Augusto Casillo",
         photo: "themes/yellow-swan/img/leonardoaugusto.jpg",
-        bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-        company: "Anfiteatro A",
+        bio: "Biografia",
+        company: "Don't worry, be happy.",
+
         presentation: {
-          title: "Palestra 13",
-          description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-          time: "15h15 às 16h00"
+   		  name: "Mesa-redonda",	
+          local: "Anfiteatro D",
+          palestrante: "Profº Dr. Leonardo Augusto Casillo",
+          time: "15h15 às 16h00",
+          description: "Descrição"
         }
       },
       {
